450f5819f315c2fec4a94590855f9d2ae4992e80
Caleb <calinduff@gmail.com>
2021-07-18 14:43:01 -0400

    initial commit and added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
+import { RecipeCardsComponent } from './recipe-cards/recipe-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownsComponent,
+    RecipeCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe-cards/recipe-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-cards',
+  templateUrl: './recipe-cards.component.html',
+  styleUrls: ['./recipe-cards.component.css']
+})
+export class RecipeCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
